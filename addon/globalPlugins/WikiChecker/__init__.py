dc350af983e1e9becc12dcc20cd081d54e28955e
Antonio Cascales <antonio.cascales@gmail.com>
2021-08-29 13:41:33 +0200

    Eliminados algunos controles de la interfaz gráfica. Modificados los eventos wx.EVT_TEXT_ENTER y wx.EVT_LISTBOX por wx.EVT_KEY_HOOK. Añadido el método onKeyEvent() para manipular los eventos del cuadro de búsqueda así como de la lista de resultados. Añadida la opción de cerrar la ventana con la tecla escape. Corregido un error que impedía buscar términos que contuviesen espacios o tildes.

--- addon/globalPlugins/WikiChecker/__init__.py
+++ addon/globalPlugins/WikiChecker/__init__.py
@@ -28,8 +28,7 @@ except:
 	pass
 
 from bs4 import BeautifulSoup
-
-from urllib import request
+from urllib import request, parse
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	@script(gesture="kb:NVDA+e")
@@ -40,57 +39,51 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 class VentanaPrincipal(wx.Dialog):
 	def __init__(self, padre, titulo):
 		super(VentanaPrincipal, self).__init__(padre, -1, titulo, size=(1000,700))
-		
 		self.panel = wx.Panel(self, wx.ID_ANY)
-		
 		boxSizer = wx.BoxSizer(wx.HORIZONTAL)
-		
 		self.etiquetaBuscarLbl = wx.StaticText(self.panel, wx.ID_ANY, "Término a buscar")
-		self.busquedaCtrl = wx.TextCtrl(self.panel, wx.ID_ANY, "", style=wx.TE_PROCESS_ENTER)
-		self.Bind(wx.EVT_TEXT_ENTER, self.onBuscar, self.busquedaCtrl)
-		
+		self.busquedaCtrl = wx.TextCtrl(self.panel, 101, "", style=wx.TE_PROCESS_ENTER)
 		self.articulosDisponiblesLbl = wx.StaticText(self.panel, wx.ID_ANY, "Artículos disponibles")
-		self.listaResultados = wx.ListBox(self.panel, wx.ID_ANY, choices=[], style=wx.LB_SINGLE)
-		self.listaResultados.Bind(wx.EVT_LISTBOX, self.onBuscarItem)
-		
-		self.areaTexto = wx.TextCtrl(self.panel, wx.ID_ANY, "", style= wx.TE_MULTILINE | wx.TE_READONLY)
-		
-		self.aceptarBtn = wx.Button(self.panel, wx.ID_OK, "Aceptar")
-		
-		self.cancelarBtn = wx.Button(self.panel, wx.ID_CANCEL, "Cancelar")
-		
+		self.listaResultados = wx.ListBox(self.panel, 102, choices=[], style=wx.LB_SINGLE)
 		self.panel.SetSizer(boxSizer)
-	
-	def onBuscar(self, event):
-		termino = self.busquedaCtrl.GetValue()
-		self.obtenerInformacion(termino)
-		event.Skip()
-	
+		self.Bind(wx.EVT_CHAR_HOOK, self.OnKeyEvent)
+
+	def OnKeyEvent(self, event):
+		if event.GetUnicodeKey() == wx.WXK_RETURN:
+			foco = wx.Window.FindFocus().GetId()
+			if foco == 101:
+				termino = self.busquedaCtrl.GetValue()
+				self.obtenerInformacion(termino)
+				event.Skip()
+			elif foco == 102:
+				pageidSeleccionado = self.resultados[self.listaResultados.GetSelection()].getPageid()
+				wx.CallAfter(self.obtenerArticulo, pageidSeleccionado)
+		if event.GetUnicodeKey() == wx.WXK_ESCAPE:
+			self.Close()
+		else:
+			event.Skip()
+
 	def eliminarEtiquetas(self, texto):
 		return re.sub(r'<[^>]*?>', '', texto)
-	
+
 	def obtenerInformacion(self, termino):
-		req = request.Request("https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch=" + termino, data=None, headers={"User-Agent": "Mozilla/5.0"})
+		req = request.Request("https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch=" + request.quote(termino), data=None, headers={"User-Agent": "Mozilla/5.0"})
 		html = request.urlopen(req)
 		datos = html.read().decode("utf-8")
 		diccionario = json.loads(datos)
 		info = diccionario["query"]["search"]
 		self.resultados = []
 		self.listaResultados.Clear()
-		
 		for i in info:
 			resultado = Resultado(i["title"], self.eliminarEtiquetas(i["snippet"]), i["pageid"])
 			self.resultados.append(resultado)
 			self.listaResultados.AppendItems(resultado.__str__())
-		
 		self.listaResultados.SetFocus()
-	
+
 	def onBuscarItem(self, event):
 		opcion = self.listaResultados.GetSelection()
-		pageid = self.resultados[opcion].getPageid()
-		
-		wx.CallAfter(self.obtenerArticulo, pageid)
-	
+		self.pageid = self.resultados[opcion].getPageid()
+
 	def obtenerArticulo(self, pageid):
 		hilo = HiloConsulta(self, pageid)
 		hilo.start()
@@ -100,34 +93,26 @@ class Resultado():
 		self.title = title
 		self.snippet = snippet
 		self.pageid = pageid
-	
+
 	def getTitle(self):
 		return self.title
-	
+
 	def getSnippet(self):
 		return self.snippet
-	
+
 	def getPageid(self):
 		return self.pageid
-	
+
 	def __str__(self):
 		return self.title + ": " + self.snippet
 
 class HiloConsulta(Thread):
 	def __init__(self, padre, pageid):
 		super(HiloConsulta, self).__init__()
-		
 		self.daemon = True
 		self.padre = padre
 		self.pageid = pageid
-	
+
 	def run(self):
-#		self.padre.areaTexto.Clear()
-#		
 		url = "https://es.wikipedia.org/?curid=" + str(self.pageid)
-#		req = request.Request(url, data=None, headers={"User-Agent": "Mozilla/5.0"})
-#		html = request.urlopen(req)
-#		datos = html.read().decode("utf-8")
-#		
-#		self.padre.areaTexto.write(self.padre.eliminarEtiquetas(datos))
 		webbrowser.open(url)
