eb03618c7702b54b645681b52fb94d1919d7fa56
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-01-05 19:14:39 +0100

    La recarga de plugins se maneja adecuadamente de nuevo. Para ello, se hace uso de globalVars

--- addon/globalPlugins/WikiChecker/__init__.py
+++ addon/globalPlugins/WikiChecker/__init__.py
@@ -28,8 +28,10 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			return
 
 		self.mainWindow = MainWindow(gui.mainFrame, _("WikiChecker - Ventana Principal"))
-
+		if hasattr(globalVars, 'wikiChecker'):
+			self.postStartupHandler()
 		core.postNvdaStartup.register(self.postStartupHandler)
+		globalVars.wikiChecker = None
 
 	@script(gesture=None, description=_("Busca los artículos relacionados con el término introducido en Wikipedia."), category=_("WikiChecker"))
 	def script_checkWikiTerm(self, gesture):
